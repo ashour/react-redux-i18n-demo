6a96797b6bd7cd17fc48da160bf3b70df263de8b
Mohammad Ashour <ashour.mohammad@gmail.com>
2019-12-18 19:36:56 +0100

    Added comment count to CommentList

--- src/components/Comments/CommentList.js
+++ src/components/Comments/CommentList.js
@@ -1,91 +1,101 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import Heading from 'react-bulma-components/lib/components/heading';
-import Loader from '../UI/Loader';
-import CommentCard from './CommentCard';
-import CommentForm from './CommentForm';
-import { setCommentsForConcert } from '../../redux/actions/comments';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Heading from "react-bulma-components/lib/components/heading";
+import Loader from "../UI/Loader";
+import CommentCard from "./CommentCard";
+import CommentForm from "./CommentForm";
+import { setCommentsForConcert } from "../../redux/actions/comments";
 
 class CommentList extends React.Component {
-	state = {
-		isLoading: true,
-	}
+  state = {
+    isLoading: true,
+  };
 
-	fetchComments() {
-		const { activeConcertId } = this.props;
+  fetchComments() {
+    const { activeConcertId } = this.props;
 
-		if (typeof activeConcertId !== 'number') { return; }
+    if (typeof activeConcertId !== "number") {
+      return;
+    }
 
-		const commentList = this.props.commentMap[activeConcertId];
+    const commentList = this.props.commentMap[activeConcertId];
 
-		if (commentList && commentList.length > 0) { return; }
+    if (commentList && commentList.length > 0) {
+      return;
+    }
 
-		this.setState({ isLoading: true });
+    this.setState({ isLoading: true });
 
-		fetch(`/data/comments/${activeConcertId}.json`)
-			.then(response => response.json())
-			.then((comments) => {
-				this.props.setCommentsForConcert(activeConcertId, comments);
+    fetch(`/data/comments/${activeConcertId}.json`)
+      .then(response => response.json())
+      .then(comments => {
+        this.props.setCommentsForConcert(activeConcertId, comments);
 
-				this.setState({ isLoading: false });
-			});
-	}
+        this.setState({ isLoading: false });
+      });
+  }
 
-	componentDidMount() {
-		this.fetchComments();
-	}
+  getCommentList() {
+    return this.props.commentMap[this.props.activeConcertId];
+  }
 
-	componentDidUpdate(prevProps) {
-		if (this.props.activeConcertId !== prevProps.activeConcertId) {
-			this.fetchComments();
-		}
-	}
+  componentDidMount() {
+    this.fetchComments();
+  }
 
-	renderHeader() {
-		return (
-			<Heading subtitle size={4} renderAs="h3">
-				Comments
-			</Heading>
-		);
-	}
+  componentDidUpdate(prevProps) {
+    if (this.props.activeConcertId !== prevProps.activeConcertId) {
+      this.fetchComments();
+    }
+  }
 
-	renderContent() {
-		const comments = this.props.commentMap[this.props.activeConcertId];
+  renderHeader() {
+    const comments = this.getCommentList();
 
-		return comments
-			? comments.map(comment => (
-				<CommentCard key={comment.id} comment={comment} />
-			))
-			: null;
-	}
+    return (
+      <Heading subtitle size={4} renderAs="h3">
+        {comments ? comments.length : 0} comments
+      </Heading>
+    );
+  }
 
-	renderWithLoadingIndicator() {
-		return this.state.isLoading ? <Loader /> : this.renderContent();
-	}
+  renderContent() {
+    const comments = this.getCommentList();
 
-	render() {
-		return (
-			<>
-				{this.renderHeader()}
+    return comments
+      ? comments.map(comment => (
+          <CommentCard key={comment.id} comment={comment} />
+        ))
+      : null;
+  }
 
-				<CommentForm />
+  renderWithLoadingIndicator() {
+    return this.state.isLoading ? <Loader /> : this.renderContent();
+  }
 
-				{this.renderWithLoadingIndicator()}
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        {this.renderHeader()}
+
+        <CommentForm />
+
+        {this.renderWithLoadingIndicator()}
+      </>
+    );
+  }
 }
 
 CommentList.propTypes = {
-	activeConcertId: PropTypes.number,
-	commentMap: PropTypes.object.isRequired,
-	setCommentsForConcert: PropTypes.func.isRequired,
+  activeConcertId: PropTypes.number,
+  commentMap: PropTypes.object.isRequired,
+  setCommentsForConcert: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-	commentMap: state.comments.commentMap,
-	activeConcertId: state.concerts.activeConcertId,
+const mapStateToProps = state => ({
+  commentMap: state.comments.commentMap,
+  activeConcertId: state.concerts.activeConcertId,
 });
 
 const mapDispatchToProps = { setCommentsForConcert };
